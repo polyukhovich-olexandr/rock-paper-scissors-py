3dd85993b03277b6d2981af64c9353e9c76c10dd
polyukhovich-olexandr <leanl3012@gmail.com>
2024-02-08 00:16:55 +0200

    add comments, type hints, description string

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,30 +1,37 @@
 import random
 
+# Перелік дій
+ACTIONS: dict[int, str] = {0: "Rock", 1: "Paper", 2: "Scissors"}
 
-ACTIONS = {0: "Rock", 1: "Paper", 2: "Scissors"}
-VICTORIES = {
-    "Rock": "Scissors",  # Rock beats scissors
-    "Paper": "Rock",  # Paper beats rock
-    "Scissors": "Paper",  # Scissors beats paper
+# Перелік переможних варіантів, де ключ - переможець, а значення - програшник
+VICTORIES: dict[str, str] = {
+    "Rock": "Scissors",  # Камінь перемагає ножиці
+    "Paper": "Rock",  # Папір перемагає камінь
+    "Scissors": "Paper",  # Ножиці перемагають папір
 }
 
 
-def get_user_selection(actions):
-    choices = [f"{actions[action]}[{action}]" for action in actions]
-    choices_str = ", ".join(choices)
-    selection = int(input(f"Enter a choice ({choices_str}): "))
-    action = actions[selection]
+def get_user_selection(actions: dict[int, str]) -> str:
+    """Отримує вибір користувача та повертає дію у вигляді рядка."""
+    choices: list[str] = [f"{actions[action]}[{action}]" for action in actions]
+    choices_str: str = ", ".join(choices)
+    selection: int = int(input(f"Enter a choice ({choices_str}): "))
+    action: str = actions[selection]
     return action
 
 
-def get_computer_selection(actions):
-    selection = random.randint(0, len(actions) - 1)
-    action = actions[selection]
+def get_computer_selection(actions: dict[int, str]) -> str:
+    """Генерує випадковий вибір комп'ютера та повертає дію у вигляді рядка."""
+    selection: int = random.randint(0, len(actions) - 1)
+    action: str = actions[selection]
     return action
 
 
-def get_determine_winner(victories, user_action, computer_action):
-    defeats = victories[user_action]
+def get_determine_winner(
+    victories: dict[str, str], user_action: str, computer_action: str
+) -> str:
+    """Визначає переможця гри та повертає результат."""
+    defeats: str = victories[user_action]
     if user_action == computer_action:
         result = f"Both players selected {user_action}. It's a tie!"
     elif computer_action in defeats:
@@ -35,21 +42,28 @@ def get_determine_winner(victories, user_action, computer_action):
 
 
 if __name__ == "__main__":
+    # Ігри йдуть вічно одна за одною
     while True:
         try:
-            user_selection = get_user_selection(ACTIONS)
+            # Отримується вибір користувача
+            user_selection: str = get_user_selection(ACTIONS)
             print(user_selection)
-            computer_selection = get_computer_selection(ACTIONS)
+            # Отримується випадковий вибір комп'ютера
+            computer_selection: str = get_computer_selection(ACTIONS)
             print(computer_selection)
-            determine_winner = get_determine_winner(
+            # Визначення переможця гри
+            determine_winner: str = get_determine_winner(
                 VICTORIES, user_selection, computer_selection
             )
+            # Виводить результат гри
             print(determine_winner)
+        # Якщо з'явилася помилка, або вибрано не те, що можна було
         except:
-            range_str = f"[0, {len(ACTIONS) - 1}]"
+            range_str: str = f"[0, {len(ACTIONS) - 1}]"
             print(f"Invalid selection. Enter a value in range {range_str}")
+            # Гра починається спочатку, адже обрав він неіснуючий варіант
             continue
-
-        play_again = input("Play again? (y/n): ")
+        # Якщо гра пройшла успішно, запитуємо чи ще хоче гравець грати?
+        play_again: str = input("Play again? (y/n): ")
         if play_again.lower() != "y":
             break
